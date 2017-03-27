b16cd58b4ffda12c3f6f8cedbf3344cabef91a28
Carlos <bobcn_1991@hotmail.com>
2017-03-27 08:11:17 +0100

    UI

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -16,7 +16,7 @@ AppComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         selector: 'my-app',
-        template: '<h1>hello world</h1>'
+        templateUrl: 'app.component.html'
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
