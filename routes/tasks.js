463054093ae8c48bae5fb9b77efa75c51dd204f9
Carlos <bobcn_1991@hotmail.com>
2017-03-27 05:18:59 +0100

    API REST and MongoDB connection
    API REST or CRUD I'm not sure :+1:

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,82 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://carlos:carlos@ds137540.mlab.com:37540/mytaskslist_carlos', ['tasks']);
 
+
+//GET ALL TASKS
 router.get('/tasks', function(req, res, next){
-    res.send('TASKS API');
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+//GET A SINGLE TASK
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+//SAVE TASKS (POST)
+router.post('/task', function(req, res, next){
+    var task = req.body;
+    if(!task.title || (task.isDone + '')){
+        res.status(400);
+        res.json({
+            "error":"Bad Data"
+        });
+    } else{
+        db.tasks.save(task, function(err, task){
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
+});
+
+//DELETE TASK
+router.delete('/task/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+//UPDATE TASK
+router.put('/task/:id', function(req, res, next){
+    var task = req.body;
+    var updTask = {};
+
+    if(task.isDone){
+        updTask.isDone = task.isDone;
+    }
+    if(task.title){
+        updTask.title = task.title;
+    }
+    if(!updTask){
+        res.status(400);
+        res.json({ "error":"Bad Data"});
+    } else{
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask,{},function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+
+    }
+
+
 });
 
 module.exports = router;
