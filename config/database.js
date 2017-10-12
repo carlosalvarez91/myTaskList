53f94e5f9a96d3e8346c8728db4661a2d2ff8ebf
carlos <bobcn_1991@hotmail.com>
2017-10-12 10:28:30 +0100

    mLab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  //database: 'mongodb://localhost:27017/meanauth',
+  database:'mongodb://Carlos:mypass4321!@ds117965.mlab.com:17965/mypass',
   secret: 'yoursecret'
 }
